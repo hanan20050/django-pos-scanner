76f54eb22088a56eb8684ffd0977a3091470f82a
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-23 21:46:04 +0800

    refactor: refactor the product model to add a minimum stock level

--- accounts/models.py
+++ accounts/models.py
@@ -76,6 +76,7 @@ class Product(models.Model):
     category = models.CharField(max_length=100, choices=CATEGORIES, default='Laptop')
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
     barcode = models.CharField(max_length=100, unique=True)
+    min_stock_level = models.PositiveIntegerField(default=3)
 
     def __str__(self):
         return f"{self.product_name} | {self.barcode}"
