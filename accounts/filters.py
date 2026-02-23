77ed28d22844c8484e3ebc57ea2b71322158ebad
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-23 20:08:36 +0800

    refactor: refactor the form filtering

--- accounts/filters.py
+++ accounts/filters.py
@@ -2,6 +2,9 @@ import django_filters
 from .models import *
 
 class InventoryFilter(django_filters.FilterSet):
+    product__product_name = django_filters.CharFilter(field_name='product__product_name', lookup_expr='icontains', label='Product Name')
+    product__barcode = django_filters.CharFilter(field_name='product__barcode', lookup_expr='icontains', label='Barcode')
+
     class Meta:
         model = BranchInventory
-        fields = '__all__'
+        fields = ['branch','product']
