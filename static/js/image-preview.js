4fd7576fdb2a0ff5891666395df8a66002d7f3f7
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-26 18:08:45 +0800

    feat: added the preview feature on the form

--- static/js/image-preview.js
+++ static/js/image-preview.js
@@ -1,1 +1,19 @@
-const imagePreview = document.querySelector('input[type="file"]')
+document.addEventListener('DOMContentLoaded', () => {
+    const imageInput = document.querySelector('input[type="file"]')
+    const displayImage = document.querySelector('#image-preview')
+    const svg = document.querySelector('#svg')
+
+    imageInput.addEventListener('change', (event) => {
+        const imgObject = event.target.files[0]
+
+        if (imgObject){
+            displayImage.src = URL.createObjectURL(imgObject)
+            displayImage.classList.remove('hidden')
+            svg.classList.add('hidden')
+        } else {
+            displayImage.classList.add('hidden')
+            svg.classList.remove('hidden')
+        }
+
+    })
+});
