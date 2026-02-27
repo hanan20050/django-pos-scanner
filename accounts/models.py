6ef020ae146a44d57f58fcd70d31f4ebe6631b82
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-02-27 22:41:41 +0800

    refactor: added the image on the product model to be able to upload product image on admin

--- accounts/models.py
+++ accounts/models.py
@@ -80,6 +80,7 @@ class Product(models.Model):
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
     barcode = models.CharField(max_length=100, unique=True)
     min_stock_level = models.PositiveIntegerField(default=3)
+    image = models.ImageField(upload_to='media/', null=True, blank=True)
 
     def __str__(self):
         return f"{self.product_name} | {self.barcode}"
