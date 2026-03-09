5bc66cf83c0fccf1f03ed1da010a0b13cd2c7b1c
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-03-09 22:20:02 +0800

    fix(bug): Deleting order in admin
    Fix the bug to be able to delete an order in admin by returning the correct class attrib name

--- accounts/models.py
+++ accounts/models.py
@@ -126,7 +126,7 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.quantity} | {self.product.name}"
+        return f"{self.quantity} | {self.product.product_name}"
 
 class Payment(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
