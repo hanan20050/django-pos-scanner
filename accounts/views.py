9e4b3682596e0c056bca1e2274e27cc678b4d9ef
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-03-18 00:02:51 +0800

    test(employee): Employee sales, names and total
    Get the employee sales, names and total to be displayed in a bar chart

--- accounts/views.py
+++ accounts/views.py
@@ -104,11 +104,30 @@ def admin_reports(request):
         total=Sum('total_amount')
     ).order_by('-total')
 
-    # 2. Initialize empty lists (The Safety Net)
+
     branch_names = []
     branch_totals = []
 
-    # 3. Populate only if data exists
+    if branch_query.exists():
+        for data_row in branch_query:
+            name = data_row['branch__name'] if data_row['branch__name'] else "Main Store"
+            amount = float(data_row['total']) if data_row['total'] else 0.0
+
+            branch_names.append(name)
+            branch_totals.append(amount)
+
+
+    employee_sales = Order.objects.values('employee__name').annotate(
+        total=Sum('total_amount')
+    ).order_by('-total')
+
+    employee_names = [
+        f"{item['employee__name']}" if item['employee__name'] else "System Admin"
+        for items in employee_sales
+    ]
+
+    employee_totals = [float(item['total'] or 0) for item in employee_sales]
+
     if branch_query.exists():
         for data_row in branch_query:
             name = data_row['branch__name'] if data_row['branch__name'] else "Main Store"
@@ -142,7 +161,8 @@ def admin_reports(request):
     page_obj = paginator.get_page(page_number)
 
     context = {'gross_revenue': gross_revenue, 'cost': cost, 'net_profit': net_profit, 'aov': aov, 'ratio': ratio, 'outstanding_balance': outstanding_balance, 'total_transactions': total_transactions, 'transactions': transactions, 'page_obj': page_obj, 'cash_total': cash_total, 'installment_total': installment_total, 'branch_names': branch_names,
-    'branch_totals': branch_totals,}
+    'branch_totals': branch_totals, 'employee_names': employee_names,
+    'employee_totals': employee_totals,}
 
     print(gross_revenue)
 
