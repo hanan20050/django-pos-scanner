fbcc7e91807a08356e88470d31dab65b1cc7655d
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-03-17 22:07:39 +0800

    fix(amount): Amount value
    Humanize amount value

--- pos/settings.py
+++ pos/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'django_filters'
+    'django_filters',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
