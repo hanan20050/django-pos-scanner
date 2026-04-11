c0cd03e1195236a4a1f1050bf233150fc6cb3dbc
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-04-11 21:48:10 +0800

    fix: yml file to run env and secret key in settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -24,7 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-ci-checks')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
