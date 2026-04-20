8b7150ef65e8578cc200d5e98563e050f64350ac
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-04-20 23:01:57 +0800

    fix: Remove tkinter import from signals

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,3 @@
-from idlelib.configdialog import changes
-
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
