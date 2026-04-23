1ce913449c48128056195c4699ccafd19136bab0
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2026-04-24 01:54:48 +0800

    fix(path): media re_path
     repath for media

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.urls import re_path
 
 urlpatterns = [
     path('', views.home, name='home'),
