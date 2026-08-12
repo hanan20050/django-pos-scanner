acd6c45973bc3ea3fafdf5fcea9762e6e807936c
hanan20050 <hananbuttar2005@gmail.com>
2026-08-13 00:32:55 +0500

    Fix database migration issue on Render: Move migrations to start.sh so they run after persistent disk is mounted

--- build.sh
+++ build.sh
@@ -5,34 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
 
-python manage.py shell -c "
-import os
-from django.contrib.auth.models import User
-from accounts.models import Employee, Branch
-
-# Superuser / Admin
-admin_user = os.getenv('ADMIN_USERNAME', 'admin')
-admin_pass = os.getenv('ADMIN_PASSWORD', 'admin')
-u_admin, created = User.objects.get_or_create(username=admin_user, defaults={'email': 'admin@pharmacy.pk', 'is_staff': True, 'is_superuser': True})
-u_admin.set_password(admin_pass)
-u_admin.is_staff = True
-u_admin.is_superuser = True
-u_admin.save()
-
-# Cashier User
-cashier_user = os.getenv('CASHIER_USERNAME', 'cashier1')
-cashier_pass = os.getenv('CASHIER_PASSWORD', 'cashier123')
-branch = Branch.objects.first()
-u, created = User.objects.get_or_create(username=cashier_user, defaults={'email': 'cashier@pharmacy.pk'})
-if created or not u.check_password(cashier_pass):
-    u.set_password(cashier_pass)
-    u.save()
-
-if branch:
-    emp, _ = Employee.objects.get_or_create(user=u, defaults={'branch': branch, 'name': 'Render Cashier', 'role': 'Cashier', 'email': u.email})
-    emp.role = 'Cashier'
-    emp.branch = branch
-    emp.save()
-"
